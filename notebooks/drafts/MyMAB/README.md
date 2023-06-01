f0d7c369dbedfbade4296a512c1920621d9d92e2
Filipo S. Perotto <filipo.perotto@onera.fr>
2023-06-01 10:37:07 +0200

    Erasing draft folder
    Signed-off-by: Filipo S. Perotto <filipo.perotto@onera.fr>

--- notebooks/drafts/MyMAB/README.md
+++
@@ -1,4 +0,0 @@
-# CyMAB
-Cybernetic Multiarmed Bandits
-
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/icmlanonymous/CyMAB/master)
--- notebooks/drafts/MyMAB/setup.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-#necessary for importing local packages
-folders = ['../../packages/smab/smab/', '../../packages/mymab/']
-for folder in folders:
-    if folder not in sys.path:
-        sys.path.insert(0, folder)
